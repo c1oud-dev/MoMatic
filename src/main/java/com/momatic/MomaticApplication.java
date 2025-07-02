6a106375a29ae8f6faf0a2320289a2321e51d4f7
c1oud-dev <gksmf0187@gmail.com>
2025-07-02 14:33:54 +0900

    ⭐ Start Project : 2025-07-02

---
+++ src/main/java/com/momatic/MomaticApplication.java
@@ -0,0 +1,13 @@
+package com.momatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomaticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/momatic/MomaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
