89aa1dfd7e958411b159ee98111e2221ae3dbee0
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2026-07-21 13:32:32 +0900

    fix: Google OIDC 로그인 연동 및 로컬 MySQL 환경 설정

--- src/main/java/com/momatic/domain/user/service/CustomOAuth2UserService.java
+++ src/main/java/com/momatic/domain/user/service/CustomOAuth2UserService.java
@@ -1,45 +1,33 @@
 package com.momatic.domain.user.service;
 
-import com.momatic.domain.user.entity.User;
-import com.momatic.domain.user.repository.UserRepository;
-import java.util.Map;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
 
-/** Google OAuth2 사용자 정보를 동기화하는 서비스입니다. */
+/** Google OIDC 사용자 정보를 동기화하는 서비스입니다. */
 @Service
 @RequiredArgsConstructor
-public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+public class CustomOAuth2UserService extends OidcUserService {
 
-    private final UserRepository userRepository;
+    private final UserSyncService userSyncService;
 
     /**
-     * OAuth2 사용자 요청을 처리하고 로컬 사용자 정보를 갱신합니다.
+     * OIDC 사용자 요청을 처리하고 로컬 사용자 정보를 갱신합니다.
      *
-     * @param userRequest OAuth2 사용자 요청
-     * @return 애플리케이션에서 사용할 OAuth2 사용자 객체
-     * @throws OAuth2AuthenticationException OAuth2 인증 처리 중 오류가 발생한 경우
+     * @param userRequest OIDC 사용자 요청
+     * @return 애플리케이션에서 사용할 OIDC 사용자 객체
+     * @throws OAuth2AuthenticationException 인증 처리 중 오류가 발생한 경우
      */
     @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User oAuth2User = super.loadUser(userRequest);
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-        String email = String.valueOf(attributes.get("email"));
-        String name = String.valueOf(attributes.get("name"));
-
-        userRepository.findByEmail(email)
-                .map(found -> {
-                    found.updateProfile(name);
-                    return userRepository.save(found);
-                })
-                .orElseGet(() -> userRepository.save(User.create(email, name, "ROLE_USER", "google", email)));
-
-        return new DefaultOAuth2User(oAuth2User.getAuthorities(), attributes, "email");
+    public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+        OidcUser oidcUser = super.loadUser(userRequest);
+        String email = oidcUser.getEmail();
+        String name = oidcUser.getFullName();
+        userSyncService.syncUser(email, name);
+        return oidcUser;
     }
 }
